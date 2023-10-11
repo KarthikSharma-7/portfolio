77e2fe08ac16d0d827a334abd1a4a9b7157ad0d1
KarthikSharma-7 <84434593+KarthikSharma-7@users.noreply.github.com>
2023-10-11 19:00:28 +0530

    fix hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a Fullstack Developer and, a <br className='sm:block hidden' />
-            Competative Programmer
+            Competitive Programmer
           </p>
         </div>
       </div>
